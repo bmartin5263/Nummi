4373380f3c915956274f6af3ae29383a3024d257
Brandon <bsm2112@yahoo.com>
2023-01-17 11:38:51 -0600

    add migrations

---
+++ Nummi/Core/Database/Migrations/20230117010833_AddCandlestickTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nummi.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCandlestickTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HistoricalMinuteCandlestick",
+                columns: table => new
+                {
+                    Symbol = table.Column<string>(type: "TEXT", nullable: false),
+                    OpenTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Open = table.Column<decimal>(type: "TEXT", nullable: false),
+                    High = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Low = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Close = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Volume = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistoricalMinuteCandlestick", x => new { x.Symbol, x.OpenTime });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HistoricalMinuteCandlestick");
+        }
+    }
+}
---
+++ Nummi/Core/Database/Migrations/20230117011817_ChangeCandlestickKey.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nummi.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCandlestickKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HistoricalMinuteCandlestick",
+                table: "HistoricalMinuteCandlestick");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HistoricalMinuteCandlestick",
+                table: "HistoricalMinuteCandlestick",
+                columns: new[] { "Symbol", "OpenTimeEpoch" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HistoricalMinuteCandlestick",
+                table: "HistoricalMinuteCandlestick");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HistoricalMinuteCandlestick",
+                table: "HistoricalMinuteCandlestick",
+                columns: new[] { "Symbol", "OpenTime" });
+        }
+    }
+}
