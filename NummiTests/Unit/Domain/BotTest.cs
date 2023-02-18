4bde16d90f3cd00d6fbb04ef37b00c8debe686e3
Brandon <bsm2112@yahoo.com>
2023-02-18 13:36:19 -0600

    use Assert.Multiple

--- NummiTests/Unit/Domain/BotTest.cs
+++ NummiTests/Unit/Domain/BotTest.cs
@@ -5,17 +5,20 @@ namespace NummiTests.Unit.Domain;
 public class BotTest {
 
     [Test]
-    public void CreateBot_ShouldHaveDefaultValuesSet() {
+    public void CreateBot_ShouldHaveDefaultValuesSet()
+    {
         var bot = new Bot(name: "Jerry", mode: TradingMode.Live, funds: 100.0m);
-        
-        Assert.That(bot.Id, Is.Not.Null);
-        Assert.That(bot.Name, Is.EqualTo("Jerry"));
-        Assert.That(bot.Funds, Is.EqualTo(100.0m));
-        Assert.That(bot.Mode, Is.EqualTo(TradingMode.Live));
-        Assert.That(bot.InErrorState, Is.False);
-        Assert.That(bot.CurrentActivation, Is.Null);
-        Assert.That(bot.IsActive, Is.False);
-        Assert.That(bot.ActivationHistory, Is.Empty);
+        Assert.Multiple(() =>
+        {
+            Assert.That(bot.Id, Is.Not.Null);
+            Assert.That(bot.Name, Is.EqualTo("Jerry"));
+            Assert.That(bot.Funds, Is.EqualTo(100.0m));
+            Assert.That(bot.Mode, Is.EqualTo(TradingMode.Live));
+            Assert.That(bot.InErrorState, Is.False);
+            Assert.That(bot.CurrentActivation, Is.Null);
+            Assert.That(bot.IsActive, Is.False);
+            Assert.That(bot.ActivationHistory, Is.Empty);
+        });
     }
 
     [Test]
